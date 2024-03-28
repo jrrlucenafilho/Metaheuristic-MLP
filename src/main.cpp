1f5584aa58aa018e4d5e2138197f5b54c274d55e
jrrlucenafilho <jrrlucenafilho@gmail.com>
2024-03-28 12:59:26 -0300

    Adapted ILS to MLP

--- src/main.cpp
+++ src/main.cpp
@@ -79,17 +79,17 @@ void PrintNBSTimers()
  * @param solution
  * @param subseqMatrix Matrix holding all subseq info in solution seq
 **/
-void UpdateAllSubSeqs(Solution* solution, vector<vector<Subsequence>>& subseqMatrix, double** m)
+void UpdateAllSubSeqs(Solution& solution, vector<vector<Subsequence>>& subseqMatrix, double** m)
 {
-    int solNodeQuant = solution->sequence.size();
+    int solNodeQuant = solution.sequence.size();
 
     //First, deals with subseqs comprised of only 1 node
     for(int i = 0; i < solNodeQuant; i++){
         subseqMatrix[i][i].duration = (i > 0);
         subseqMatrix[i][i].accumulatedCost = 0;
         subseqMatrix[i][i].delayCost = 0;
-        subseqMatrix[i][i].firstNode = solution->sequence[i];
-        subseqMatrix[i][i].lastNode = solution->sequence[i];
+        subseqMatrix[i][i].firstNode = solution.sequence[i];
+        subseqMatrix[i][i].lastNode = solution.sequence[i];
     }
 
     //Now deal with the non-single-node subseqs
@@ -569,12 +569,12 @@ int BoundedRand(int min, int max)
 }
 
 //Return Solution, may have to call a cost-calc function inside here
-Solution Disturbance(Solution& tspSol, double** m, int dimension)
+Solution Disturbance(Solution& solution, vector<vector<Subsequence>>& subseqMatrix, double** m, int dimension)
 {
     disturbance_time_ptr->beginTime = std::clock();
 
-    vector<int> copiedSequence = tspSol.sequence;
-    int segmentMaxLength = ceil(dimension / 10.0);
+    vector<int> copiedSeq = solution.sequence;
+    int subseqMaxLength = ceil(dimension / 10.0);
     Solution disturbedSol;
 
     //Will mark the index of first and last elements of each subsequence
@@ -585,14 +585,14 @@ Solution Disturbance(Solution& tspSol, double** m, int dimension)
     //Length of each subsequence and of the space among them
     int subseq2Length, inbetweenSubseqsLength; //subseq2Length uneeded
 
-    subseq1Index_begin = BoundedRand(1, dimension - segmentMaxLength - segmentMaxLength - 1);
-    subseq1Index_end = BoundedRand(subseq1Index_begin + 1, subseq1Index_begin + segmentMaxLength - 1);
-    subseq2Index_begin = BoundedRand(subseq1Index_end + 1, dimension - segmentMaxLength);
-    subseq2Index_end = BoundedRand(subseq2Index_begin + 1, subseq2Index_begin + segmentMaxLength - 1);
+    subseq1Index_begin = BoundedRand(1, dimension - subseqMaxLength - subseqMaxLength - 1);
+    subseq1Index_end = BoundedRand(subseq1Index_begin + 1, subseq1Index_begin + subseqMaxLength - 1);
+    subseq2Index_begin = BoundedRand(subseq1Index_end + 1, dimension - subseqMaxLength);
+    subseq2Index_end = BoundedRand(subseq2Index_begin + 1, subseq2Index_begin + subseqMaxLength - 1);
 
     //Actually making the subsequences from rand calc'd indexes
-    vector<int> subseq1(copiedSequence.begin() + subseq1Index_begin, copiedSequence.begin() + subseq1Index_end);
-    vector<int> subseq2(copiedSequence.begin() + subseq2Index_begin, copiedSequence.begin() + subseq2Index_end);
+    vector<int> subseq1(copiedSeq.begin() + subseq1Index_begin, copiedSeq.begin() + subseq1Index_end);
+    vector<int> subseq2(copiedSeq.begin() + subseq2Index_begin, copiedSeq.begin() + subseq2Index_end);
 
     //Lengths and space calc
     //subseq1Length = subseq1Index_end - subseq1Index_begin;
@@ -601,15 +601,15 @@ Solution Disturbance(Solution& tspSol, double** m, int dimension)
 
     //Disturbing copied sequence
     //Swapping these not-necessarily-equally-sized subseqs in the main sequence
-    copiedSequence.erase(copiedSequence.begin() + subseq1Index_begin, copiedSequence.begin() + subseq1Index_end);
-    copiedSequence.insert(copiedSequence.begin() + subseq1Index_begin, subseq2.begin(), subseq2.end());
+    copiedSeq.erase(copiedSeq.begin() + subseq1Index_begin, copiedSeq.begin() + subseq1Index_end);
+    copiedSeq.insert(copiedSeq.begin() + subseq1Index_begin, subseq2.begin(), subseq2.end());
 
-    copiedSequence.erase(copiedSequence.begin() + subseq1Index_begin + subseq2Length + inbetweenSubseqsLength, copiedSequence.begin() + subseq1Index_begin + subseq2Length + inbetweenSubseqsLength + subseq2Length);
-    copiedSequence.insert(copiedSequence.begin() + subseq1Index_begin + subseq2Length + inbetweenSubseqsLength, subseq1.begin(), subseq1.end());
+    copiedSeq.erase(copiedSeq.begin() + subseq1Index_begin + subseq2Length + inbetweenSubseqsLength, copiedSeq.begin() + subseq1Index_begin + subseq2Length + inbetweenSubseqsLength + subseq2Length);
+    copiedSeq.insert(copiedSeq.begin() + subseq1Index_begin + subseq2Length + inbetweenSubseqsLength, subseq1.begin(), subseq1.end());
 
     //Calculating cost and attributing to disturbed solution
-    disturbedSol.cost = CalculateSequenceCost(copiedSequence, m);
-    disturbedSol.sequence = copiedSequence;
+    disturbedSol.cost = CalculateSequenceCost(copiedSeq, m);
+    disturbedSol.sequence = copiedSeq;
 
     disturbance_time_ptr->endTime = std::clock();
     disturbance_time_ptr->accumulatedTime += disturbance_time_ptr->endTime - disturbance_time_ptr->beginTime;
@@ -619,20 +619,27 @@ Solution Disturbance(Solution& tspSol, double** m, int dimension)
 
 Solution IteratedLocalSearch(int maxIters, int maxIterILS, Data& data)
 {
-    Solution bestOfAllSolution;
+    Solution bestOfAllSolution, currIterSolution, currBestSolution;
     bestOfAllSolution.cost = INFINITY;
 
     for(int i = 0; i < maxIters; i++){
         //Builds a beginning solution based on fair guesses
-        Solution currIterSolution = BuildSolution(data.getMatrixCost(), data.getDimension());
-        Solution currBestSolution = currIterSolution;
+        currIterSolution = BuildSolution(data.getMatrixCost(), data.getDimension());
+
+        vector<vector<Subsequence>> subseqMatrix(data.getDimension() + 1, vector<Subsequence>(data.getDimension() + 1));
+
+        UpdateAllSubSeqs(currIterSolution, subseqMatrix, data.getMatrixCost());
+        currBestSolution = currIterSolution;
+
+        currIterSolution.cost = subseqMatrix[0][data.getDimension()].accumulatedCost;
+        currBestSolution.cost = currIterSolution.cost; //TODO: Check if this can be skipped
 
         int iterILS = 0;
 
         while(iterILS <= maxIterILS){
             //Tries to enhance the fairly-guessed solution
             //By doing small modifications to it
-            LocalSearch(currIterSolution, data.getMatrixCost(), data.getDimension());
+            LocalSearch(currIterSolution, subseqMatrix, data.getMatrixCost(), data.getDimension());
 
             if(currIterSolution.cost < currBestSolution.cost){
                 currBestSolution = currIterSolution;
@@ -641,7 +648,7 @@ Solution IteratedLocalSearch(int maxIters, int maxIterILS, Data& data)
 
             //If not possible to make it better, shake the current best solution up a lil'
             //to see if we didn't just go into a 'local best pitfall'
-            currIterSolution = Disturbance(currBestSolution, data.getMatrixCost(), data.getDimension());
+            currIterSolution = Disturbance(currBestSolution, subseqMatrix, data.getMatrixCost(), data.getDimension());
             iterILS++;
         }
 
@@ -655,11 +662,11 @@ Solution IteratedLocalSearch(int maxIters, int maxIterILS, Data& data)
 
 int main(int argc, char** argv)
 {
-    int maxIter = 50;
+    int maxIter = 10;
     int maxIterILS;
     double costsSum = 0;
     auto data = Data(argc, argv[1]);
-    Solution tspSol;
+    Solution solution;
     my_time_t ILS_iter_time;
 
     data.read();
@@ -687,23 +694,23 @@ int main(int argc, char** argv)
 
         ILS_iter_time.beginTime = std::clock();
 
-        tspSol = IteratedLocalSearch(maxIter, maxIterILS, data);
+        solution = IteratedLocalSearch(maxIter, maxIterILS, data);
 
-        tspSol.cost = CalculateSequenceCost(tspSol.sequence, data.getMatrixCost());
-        costsSum += tspSol.cost;
+        solution.cost = CalculateSequenceCost(solution.sequence, data.getMatrixCost());
+        costsSum += solution.cost;
 
         ILS_iter_time.endTime = std::clock();
         ILS_iter_time.accumulatedTime += ILS_iter_time.endTime - ILS_iter_time.beginTime;
 
-        cout << "Cost of s (iter " << i + 1 << "): " << tspSol.cost << '\n';
+        cout << "Cost of s (iter " << i + 1 << "): " << solution.cost << '\n';
     }
 
     cout << "-------------------------------\n";
     cout << "Instance Name: " << data.getInstanceName() << '\n';
     cout << "Solution s = ";
 
-    for(size_t i = 0; i < tspSol.sequence.size() - 1; i++){
-        cout << tspSol.sequence[i] << " -> ";
+    for(size_t i = 0; i < solution.sequence.size() - 1; i++){
+        cout << solution.sequence[i] << " -> ";
     }
     cout << "1\n";
     cout << "Average s cost: " << costsSum / 10 << '\n';
